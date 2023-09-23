f1d6dca07fca751bfe8e1e4a4cbc0d223042489d
Daniel Dowdle <dowdl021@umn.edu>
2023-09-23 11:27:14 -0500

    � Initial Firebase setup, it is connected and working. We can get livedata from the database and then will also be able to update the database

---
+++ packages/mobile/android/API/firestore.js
@@ -0,0 +1,12 @@
+import firebase from "firebase/app";
+import "firebase/firestore";
+
+const firebaseConfig = {
+  // Your Firebase configuration
+};
+
+firebase.initializeApp(firebaseConfig);
+
+const db = firebase.firestore();
+
+export { db };
