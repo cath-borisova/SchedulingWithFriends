e9fc1d644f88441dde0e76b6d822f46ce9be3303
whate <whatelsetocallit@gmail.com>
2023-09-23 18:16:09 -0500

    More Lockscreen stuff

--- packages/mobile/screens/LockScreen.js
+++ packages/mobile/screens/LockScreen.js
@@ -1,13 +1,39 @@
-import { Button, SafeAreaView, Text, TextInput, View } from "react-native";
+import { Button, ImageBackground, SafeAreaView, Text, TextInput, View } from "react-native";
 import React from "react";
+
+const image = {uri: 'https://media.discordapp.net/attachments/1148331832983695370/1155279841109741648/background.png?width=2050&height=1022'};
 
 export default function LockScreen() {
 
+  let vision = true;
+  const [Username, onChangeUsername] = React.useState('');
+  const [Password, onChangePassword] = React.useState('');
+
   return (
     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+
+      <ImageBackground source={image} resizeMode="cover"/>
+
       <Text>Login</Text>
-      <Textbox placeholder="Username" />
-      <Textbox placeholder="Password" />
+      <SafeAreaView>
+        <TextInput
+          value={Username}
+          placeholder="Pick a Username!"
+          onChangeText={onChangeUsername}
+          editable={true}
+          maxLength={30}
+
+        />
+
+        <TextInput
+          value={Password}
+          placeholder="Pick a Password!"
+          onChangeText={onChangePassword}
+          editable={true}
+          maxLength={30}
+          secureTextEntry={vision}
+          />
+
       <Button
         title="Forgot Password?"
         onPress={ForgotPasswordButtonHandler}
@@ -15,7 +41,7 @@ export default function LockScreen() {
 
       <Button
         title="Login"
-        onPress={LoginButtonHandler}
+        onPress={LoginButtonHandler(Username, Password)}
       />
 
       <Text>
@@ -26,24 +52,28 @@ export default function LockScreen() {
         title="Login with google"
         onPress={GoogleSSOButtonHandler}
       />
+      </SafeAreaView>
     </View>
   );
 }
 
-const Textbox = ({string: placeholder}) => {
+/*
+const Textbox = (placeholder) => {
   const [text, onChangeText] = React.useState('');
 
     return (
       <SafeAreaView>
         <TextInput
           value={text}
-          placeholder={placeholder}
+          placeholder=placeholder
           onChangeText={onChangeText}
         />
       </SafeAreaView>
     );
 }
 
+
+ */
 const ForgotPasswordButtonHandler = () => {}
-const LoginButtonHandler = () => {}
+const LoginButtonHandler = (username, password) => {}
 const GoogleSSOButtonHandler = () => {}
