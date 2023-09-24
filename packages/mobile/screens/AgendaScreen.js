1e20b7070d5358a5154772c85bd1771d0ee2f973
Daniel Dowdle <dowdl021@umn.edu>
2023-09-24 11:26:25 -0500

    WEFef

--- packages/mobile/screens/AgendaScreen.js
+++ packages/mobile/screens/AgendaScreen.js
@@ -1,9 +1,7 @@
 import React, { Component } from "react";
 import { Alert, StyleSheet, Text, View, TouchableOpacity } from "react-native";
 import CreateEventScreen from "./CreateEventScreen";
-import {
-  Agenda,
-} from "react-native-calendars";
+import { Agenda } from "react-native-calendars";
 import testIDs from "../testIDs";
 
 class AgendaScreen extends Component {
@@ -16,6 +14,11 @@ class AgendaScreen extends Component {
   }
 
   render() {
+    const firstEvent = {
+      title: "First Event",
+      description: "This is the first event",
+    };
+
     return (
       <View style={{ flex: 1 }}>
         {this.state.showCreateEvent ? null : (
@@ -35,33 +38,96 @@ class AgendaScreen extends Component {
       </View>
     );
   }
+  getRandomTimeGap = () => {
+    //just return a random number between 1 and 4
+    return Math.floor(Math.random() * 4 + 1);
+  };
 
   loadItems = (day) => {
     const items = this.state.items || {};
+    const eventChoices = [
+      {
+        item: "Julia",
+        id: "JUVE",
+      },
+      {
+        item: "George",
+        id: "RM",
+      },
+      {
+        item: "Katya",
+        id: "BR",
+      },
+      {
+        item: "Filmore",
+        id: "PSG",
+      },
+      {
+        item: "XxXMelloettaxXx",
+        id: "FBM",
+      },
+    ];
+
+    const getRandomStartTime = () => {
+      const randomHours = Math.floor(Math.random() * 14) + 8; // Random hours between 8 and 21
+      const randomMinutes = Math.floor(Math.random() * 60); // Random minutes between 0 and 59
+      const startTime = new Date();
+      startTime.setHours(randomHours);
+      startTime.setMinutes(randomMinutes);
+      return startTime;
+    };
 
     setTimeout(() => {
       for (let i = -15; i < 85; i++) {
         const time = day.timestamp + i * 24 * 60 * 60 * 1000;
         const strTime = this.timeToString(time);
 
+        const timeGap = this.getRandomTimeGap();
+
+        //make start time a random time between 8am and 10pm
+        const randomStartTime = Math.floor(Math.random() * 14 + 8);
+        //convert that random time to an actual date
+        const startTime = new Date(randomStartTime);
+        const startTimeHours = startTime.getHours();
+
+        //Take that random time gap and add it to the current time to get the end time
+        const endTime = time + timeGap * 60 * 60 * 1000;
+        //Convert the end time to a string
+        const strEndTime = this.timeToString(endTime);
+        //start time and end time are now strings
+        //strTime and strEndTime
+
         if (!items[strTime]) {
           items[strTime] = [];
 
           const numItems = Math.floor(Math.random() * 3 + 1);
           for (let j = 0; j < numItems; j++) {
+            const randomstartTime = getRandomStartTime();
+            const startTimeHours = randomstartTime.getHours();
+
+            const randomEventChoice =
+              eventChoices[Math.floor(Math.random() * eventChoices.length)];
             items[strTime].push({
-              name: "Item for " + strTime + " #" + j,
+              name: `${randomEventChoice.item} is available!`,
               height: Math.max(50, Math.floor(Math.random() * 150)),
               day: strTime,
+              timeGap: timeGap,
+              time: randomstartTime,
+              eventId: randomEventChoice.id,
             });
           }
         }
       }
 
-      const newItems = {};
-      Object.keys(items).forEach((key) => {
-        newItems[key] = items[key];
-      });
+      const firstEvent = items[Object.keys(items)[0]][0];
+      firstEvent.item = "First event of the day";
+      firstEvent.height = 100;
+      firstEvent.timeGap = 1;
+
+      console.log(firstEvent);
+
+      const newItems = { ...items };
+
       this.setState({
         items: newItems,
       });
@@ -78,11 +144,26 @@ class AgendaScreen extends Component {
   renderItem = (reservation, isFirst) => {
     const fontSize = isFirst ? 16 : 14;
     const color = isFirst ? "black" : "#43515c";
+    const backgroundColor = getRandomColor(); // Get a random pastel color
+
+    const startTime = new Date(reservation.time);
+    const endTime = new Date(
+      startTime.getTime() + reservation.timeGap * 60 * 60 * 1000
+    );
+
+    const startTimeHours = `${startTime.getHours()}:${
+      (startTime.getMinutes() < 10 ? "0" : "") + startTime.getMinutes()
+    }`;
+    const endTimeHours = `${endTime.getHours()}:${
+      (endTime.getMinutes() < 10 ? "0" : "") + endTime.getMinutes()
+    }`;
+
+    console.log("reservation", reservation);
 
     return (
       <TouchableOpacity
         testID={testIDs.agenda.ITEM}
-        style={[styles.item, { height: reservation.height }]}
+        style={[styles.item, { height: reservation.height, backgroundColor }]}
         onPress={() =>
           this.setState({
             showCreateEvent: true,
@@ -90,7 +171,9 @@ class AgendaScreen extends Component {
           })
         }
       >
-        <Text style={{ fontSize, color }}>{reservation.name}</Text>
+        <Text
+          style={{ fontSize, color }}
+        >{`${reservation.name} (${startTimeHours} - ${endTimeHours})`}</Text>
       </TouchableOpacity>
     );
   };
@@ -102,6 +185,7 @@ class AgendaScreen extends Component {
       return (
         <CreateEventScreen
           reservation={selectedReservation}
+          eventArr={this.state.items}
           onClose={() => this.setState({ showCreateEvent: false })}
         />
       );
@@ -127,10 +211,65 @@ class AgendaScreen extends Component {
     return date.toISOString().split("T")[0];
   }
 }
+const pastelGradientColors = [
+  "#FFD1DC", // Misty Rose
+  "#FFABAB", // Pale Red
+  "#FFC3A0", // Deep Peach
+  "#FF677D", // Bubble Gum
+  "#D4A5A5", // Pastel Pink
+  "#FFBCBC", // Rosy Pink
+  "#FFDAC1", // Peach
+  "#FFB997", // Light Apricot
+  "#FFABAB", // Pale Red
+  "#FFD3B6", // Light Salmon
+  "#FFC3A0", // Deep Peach
+  "#FFB997", // Light Apricot
+  "#FFDDC1", // Apricot
+  "#E9CBA8", // Pastel Orange
+  "#FFABAB", // Pale Red
+];
+
+const gradients = [
+  // Light Blue Gradient
+  "#B4EAFB", // Light Sky Blue
+  "#ADD8E6", // Light Blue
+  "#87CEEB", // Sky Blue
+  "#87CEFA", // Light Sky Blue
+
+  // Pale Purple Gradient
+  "#E6E6FA", // Lavender
+  "#D8BFD8", // Thistle
+  "#DDA0DD", // Plum
+  "#EE82EE", // Violet
+
+  // Lavender Gradient
+  "#E6E6FA", // Lavender
+  "#D1C1E0", // Mauve
+  "#B19CD9", // Pastel Purple
+  "#9370DB", // Medium Purple
+
+  // Peach/Tan Gradient
+  "#FFDAB9", // Peach Puff
+  "#FFB366", // Light Apricot
+  "#FFA07A", // Light Salmon
+  "#FF7F50", // Coral
+];
+
+console.log(gradients);
+
+console.log(pastelGradientColors);
+
+const getRandomColor = () => {
+  return pastelGradientColors[Math.floor(Math.random() * 15)];
+};
 
 const styles = StyleSheet.create({
   item: {
     backgroundColor: "white",
+    textAlign: "center",
+    alignContent: "center",
+    justifyContent: "center",
+    alignItems: "center",
     flex: 1,
     borderRadius: 5,
     padding: 10,
--- packages/mobile/screens/CreateEventScreen.js
+++ packages/mobile/screens/CreateEventScreen.js
@@ -1,38 +1,20 @@
-import React, { useState } from 'react'
-import { Text, View, TextInput, TouchableOpacity   } from 'react-native'
-import SelectBox from 'react-native-multi-selectbox'
-import { xorBy } from 'lodash'
-
-// Options data must contain 'item' & 'id' keys
+import React, { useState } from "react";
+import { Text, View, TextInput, TouchableOpacity } from "react-native";
+import SelectBox from "react-native-multi-selectbox";
+import { xorBy } from "lodash";
 
 const K_OPTIONS = [
-  {
-    item: 'Julia',
-    id: 'JUVE',
-  },
-  {
-    item: 'George',
-    id: 'RM',
-  },
-  {
-    item: 'Katya',
-    id: 'BR',
-  },
-  {
-    item: 'Filmore',
-    id: 'PSG',
-  },
-  {
-    item: 'XxXMelloettaxXx',
-    id: 'FBM',
-  }
-]
+  { item: "Julia", id: "JUVE" },
+  { item: "George", id: "RM" },
+  { item: "Katya", id: "BR" },
+  { item: "Filmore", id: "PSG" },
+  { item: "XxXMelloettaxXx", id: "FBM" },
+];
 
-
-
-export default function CreateEventScreen( {onClose} ) {
-  const [textTitle, setTextTitle] = useState('');
-  const [textDesc, setTextDesc] = useState('');
+export default function CreateEventScreen({ onClose, eventArr, reservation }) {
+  const [textTitle, setTextTitle] = useState("");
+  const [textDesc, setTextDesc] = useState("");
+  const [selectedTeams, setSelectedTeams] = useState([]);
 
   const handleChangeTitle = (inputText) => {
     setTextTitle(inputText);
@@ -42,57 +24,58 @@ export default function CreateEventScreen( {onClose} ) {
     setTextDesc(inputText);
   };
 
-  const [selectedTeam, setSelectedTeam] = useState({})
-  const [selectedTeams, setSelectedTeams] = useState([])
+  // Calculate day of the week based on reservation time
+  const reservationDate = new Date(reservation.day);
+  const dayOfWeek = reservationDate.toLocaleDateString("en-US", {
+    weekday: "long",
+  });
+
+  const onMultiChange = (item) => {
+    setSelectedTeams((prevSelectedTeams) =>
+      xorBy([...prevSelectedTeams], [item], "id")
+    );
+  };
+
   return (
     <View style={{ margin: 30 }}>
-      <View style={{ width: '100%', alignItems: 'center' }}>
-      </View>
       <TextInput
-        style={{ fontSize: 29, paddingBottom: 20, paddingTop: 20}}
+        style={{ fontSize: 29, paddingBottom: 20, paddingTop: 20 }}
         placeholder="Add Event Name"
         onChangeText={handleChangeTitle}
         value={textTitle}
       />
       <TextInput
-        style={{ fontSize: 20, paddingBottom: 20}}
+        style={{ fontSize: 20, paddingBottom: 20 }}
         multiline={true}
         placeholder="Description"
         onChangeText={handleChangeDescription}
         value={textDesc}
       />
+      <Text style={{ fontSize: 20, paddingBottom: 10 }}>
+        Date: {dayOfWeek} - {reservationDate.getHours() % 12}:00 -{" "}
+        {(reservationDate.getHours() % 12) + 1}:00
+      </Text>
+
       <Text style={{ fontSize: 20, paddingBottom: 10 }}>Invited friends:</Text>
-      <Text style={{ fontSize: 20, paddingTop: 10}}>Bob</Text>
+      <Text style={{ fontSize: 20, paddingTop: 10 }}>Bob</Text>
+
       <View style={{ height: 40 }} />
-      <Text style={{ fontSize: 20, paddingBottom: 10 }}>Friends who are also free:</Text>
+      <Text style={{ fontSize: 20, paddingBottom: 10 }}>
+        Friends who are also free:
+      </Text>
       <SelectBox
         label="Select multiple"
         options={K_OPTIONS}
         selectedValues={selectedTeams}
-        onMultiSelect={onMultiChange()}
-        onTapClose={onMultiChange()}
+        onMultiSelect={onMultiChange}
+        onTapClose={onMultiChange}
         isMulti
         open={true}
       />
 
-      <TouchableOpacity
-        onPress={onClose}
-      >
-        <Text style={{alignSelf: 'flex-start'}}>Go Back</Text>
+      <TouchableOpacity onPress={onClose}>
+        <Text style={{ alignSelf: "flex-start" }}>Go Back</Text>
       </TouchableOpacity>
-
     </View>
-  )
-
-
-  function onMultiChange() {
-    return (item) => setSelectedTeams(xorBy(selectedTeams, [item], 'id'))
-  }
-
-  function onChange() {
-    return (val) => setSelectedTeam(val)
-  }
-
+  );
 }
-
-
